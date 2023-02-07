ef1bc3420f468a05f174854bbe846fc2510bd253
yuberee <59583743+yuberee@users.noreply.github.com>
2023-02-07 23:14:27 +0100

    Episode 5

---
+++ Episode 5/code/Yeti.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace YetiEscape;
+
+partial class Yeti : AnimatedEntity
+{
+	public SwimmingPlayer Victim;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/citizen/citizen.vmdl" );
+		Scale = 1.5f;
+
+		Position = Vector3.Forward * YetiEscape.LakeRadius;
+	}
+}
---
+++ Episode 5/code/YetiEscape.cs
@@ -0,0 +1,29 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YetiEscape;
+
+public partial class YetiEscape : GameManager
+{
+
+	public static float LakeRadius = 512f;
+
+	public override void ClientJoined( IClient cl )
+	{
+		base.ClientJoined( cl );
+
+		var pawn = new SwimmingPlayer();
+		
+		cl.Pawn = pawn;
+
+		var allSpawnPoints = Entity.All.OfType<SpawnPoint>();
+		var randomSpawnPoint = allSpawnPoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		pawn.Position = randomSpawnPoint.Position;
+	}
+
+}
