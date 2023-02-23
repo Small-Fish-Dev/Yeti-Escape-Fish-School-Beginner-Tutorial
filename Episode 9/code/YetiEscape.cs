6403f8d8a2548f89f12e487507d38e106a40dae9
yuberee <59583743+yuberee@users.noreply.github.com>
2023-02-23 23:57:01 +0100

    Episode 9

---
+++ Episode 9/code/YetiEscape.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YetiEscape;
+
+public partial class YetiEscape : GameManager
+{
+
+	public static float LakeRadius = 512f;
+
+	public YetiEscape()
+	{
+		if ( Game.IsClient )
+			new HUD();
+	}
+
+	public override void ClientJoined( IClient cl )
+	{
+		base.ClientJoined( cl );
+
+		Reset( cl );
+	}
+
+	public static void Reset( IClient cl )
+	{
+		cl.Pawn?.Delete();
+
+		var pawn = new SwimmingPlayer();
+		cl.Pawn = pawn;
+
+		var clothing = new ClothingContainer();
+		clothing.LoadFromClient( cl );
+		clothing.DressEntity( pawn );
+
+		var allSpawnPoints = Entity.All.OfType<SpawnPoint>();
+		var randomSpawnPoint = allSpawnPoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		pawn.Position = randomSpawnPoint.Position;
+	}
+
+	[ClientRpc]
+	public static void DisplayGameText( string text )
+	{
+		Event.Run( "DisplayText", text );
+	}
+}
